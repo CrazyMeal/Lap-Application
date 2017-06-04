bfb69b797d236a0e8f1283e649df701f1c0324bb
KVN <kvndesign.mail@gmail.com>
2017-06-04 17:01:52 +0200

    Initial commit

---
+++ src/main/java/fr/crazymeal/lap/LapApplication.java
@@ -0,0 +1,12 @@
+package fr.crazymeal.lap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LapApplication.class, args);
+	}
+}
